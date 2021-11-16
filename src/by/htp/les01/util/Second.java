12e23317b571d27557fe7be7fd8eef3107303be8
Siarhey <AverburhTeach@gmail.com>
2021-11-16 16:16:06 +0300

    add new class

---
+++ src/by/htp/les01/util/Second.java
@@ -0,0 +1,12 @@
+package by.htp.les01.util;
+
+public class Second {
+    public static void print (int repeat, String toPrint) {
+
+        for (int i = 0; i < repeat; i++) {
+            System.out.print(toPrint);
+        }
+        System.out.println();
+
+    }
+}
